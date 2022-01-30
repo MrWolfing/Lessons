69e5ded4431aa35b5593efec958b10dcaf091c83
iljas <iljas@DESKTOP-75S244G>
2022-01-30 14:29:47 +0300

    Добавьте файлы проекта.

---
+++ Lessons/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+            Console.WriteLine("Привет мир!!");
+        }
+    }
+}
